e9b2fee0ed4b13aa27fd4b1b170d7ac38a94bd7a
ZvezdilinAK <artem@intervolga.ru>
2019-07-20 15:43:25 +0400

    nav

--- src/blocks.04-project/order/order.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/order/order.tmpl-specs/base.bemjson.js
@@ -43,19 +43,35 @@ module.exports = [
                       {elem: 'offset', content: [
                           {block: 'h', size: 4, cls: 'mb-4', content: 'Способ доставки и оплаты'},
                           {cls: 'form-group', content: [
-                              {cls: 'btn-group w-100', content: [
-                                  {block: 'btn', content: 'Доставка'},
-                                  {block: 'btn', cls: 'btn-outline-primary', content: 'Самовывоз'},
+                              {block: 'nav', cls: 'nav-pills nav-justified', content: [
+                                  {block: 'a', cls: 'nav-item nav-link active', attrs: {'data-toggle': 'pill', 'href': '#tab_1'}, content: 'Доставка'},
+                                  {block: 'a', cls: 'nav-item nav-link', attrs: {'data-toggle': 'pill', 'href': '#tab_2'}, content: 'Самовывоз'},
                                 ]},
                             ]},
-                          {cls: 'form-group row', content: [
-                              {cls: 'col-sm-2 col-form-label', content: 'Адрес'},
-                              {cls: 'col-sm-10', content: [
-                                  {block: 'form-control'},
+                          {cls: 'tab-content', content: [
+                              {cls: 'tab-pane fade show active', attrs: {id: 'tab_1'}, content: [
+                                  {cls: 'form-group row', content: [
+                                      {cls: 'col-sm-2 col-form-label', content: 'Адрес'},
+                                      {cls: 'col-sm-10', content: [
+                                          {block: 'form-control'},
+                                        ]},
+                                    ]},
+                                  {cls: 'form-group', content: [
+                                      {block: 'custom-control', content: 'Подъем <b>350 рублей</b>'},
+                                    ]},
                                 ]},
-                            ]},
-                          {cls: 'form-group', content: [
-                              {block: 'custom-control', content: 'Подъем <b>350 рублей</b>'},
+                              {cls: 'tab-pane fade', attrs: {id: 'tab_2'}, content: [
+                                  {cls: 'form-group row', content: [
+                                      {cls: 'col-sm-2 col-form-label', content: 'Магазин'},
+                                      {cls: 'col-sm-10', content: [
+                                          {block: 'form-control', tag: 'select', content: [
+                                            'Адрес магазина 1',
+                                            'Адрес магазина 2',
+                                            'Адрес магазина 3',
+                                            ]},
+                                        ]},
+                                    ]},
+                                ]},
                             ]},
                           {tag: 'hr', cls: 'my-4'},
                           {cls: 'form-group row', content: [
